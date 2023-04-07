a7b8481469b4e9a1414d083a15f2b5994d27450f
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-07 16:32:21 -0400

    Filtering working

--- client/src/components/CakeMenu.js
+++ client/src/components/CakeMenu.js
@@ -21,25 +21,44 @@ function CakeMenu() {
         dropbtn.classList.toggle("dropdown-down");
     }
 
+    function toggleFilter(e) {
+        const selectedItem = document.querySelector("." + e.target.className + ".dropdown-content-select");
+        if (selectedItem)
+        {
+            selectedItem.classList.toggle("dropdown-content-select");
+            e.target.classList.toggle("dropdown-content-select");
+            e.target.parentElement.previousSibling.textContent = e.target.text;
+        }
+        switch(e.target.className)
+        {
+            case "toggleType dropdown-content-select":
+                setCakeType(parseInt(e.target.id.substr(1)));
+                break;
+            case "toggleSize dropdown-content-select":
+                setCakeSize(parseInt(e.target.id.substr(1)));
+                break;
+        }
+    }
+
 return(
     <div className="content">
         <div className="filter-nav">
             <div className="dropdown">
                 <div className="dropdown-button" id="btnCakeType" onClick={toggleFilterMenu}>All Types</div>
                 <div className="dropdown-content" id="listCakeType">
-                    <a onClick={(e) => setCakeType(0)} href="#">All</a>
-                    <a onClick={(e) => setCakeType(2)} href="#">Vanilla</a>
-                    <a onClick={(e) => setCakeType(3)} href="#">Chocolate</a>
-                    <a onClick={(e) => setCakeType(1)} href="#">Fruit</a>
+                    <a onClick={toggleFilter} id="t0" className="toggleType dropdown-content-select" href="#">All Types</a>
+                    <a onClick={toggleFilter} id="t1" className="toggleType" href="#">Fruit</a>
+                    <a onClick={toggleFilter} id="t2" className="toggleType" href="#">Vanilla</a>
+                    <a onClick={toggleFilter} id="t3" className="toggleType" href="#">Chocolate</a>
                 </div>
             </div>
             <div className="dropdown">
                 <div className="dropdown-button" id="btnCakeSize" onClick={toggleFilterMenu}>All Sizes</div>
                 <div className="dropdown-content" id="listCakeSize">
-                    <a onClick={(e) => setCakeSize(0)} href="#">All</a>
-                    <a onClick={(e) => setCakeSize(1)} href="#">Personal</a>
-                    <a onClick={(e) => setCakeSize(2)} href="#">Small</a>
-                    <a onClick={(e) => setCakeSize(3)} href="#">Large</a>
+                    <a onClick={toggleFilter} id="s0" className="toggleSize dropdown-content-select" href="#">All Sizes</a>
+                    <a onClick={toggleFilter} id="s1" className="toggleSize" href="#">Personal</a>
+                    <a onClick={toggleFilter} id="s2" className="toggleSize" href="#">Medium</a>
+                    <a onClick={toggleFilter} id="s3" className="toggleSize" href="#">Large</a>
                 </div>
             </div>
         </div>
