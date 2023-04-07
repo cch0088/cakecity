4ac6ad09a060df5dba8021bc50f2f741632ec2a5
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-07 14:05:42 -0400

    DB update

--- client/src/components/CakeMenu.js
+++ client/src/components/CakeMenu.js
@@ -7,6 +7,8 @@ function CakeMenu() {
     const API = "/cakes";
 
     const [cakes, setCakes] = useState([]);
+    const [cakeSize, setCakeSize] = useState(0);
+    const [cakeType, setCakeType] = useState(0);
 
     useEffect(() => {
         fetch(API).then(resp => resp.json()).then(data => setCakes(data));
@@ -25,26 +27,30 @@ return(
             <div className="dropdown">
                 <div className="dropdown-button" id="btnCakeType" onClick={toggleFilterMenu}>All Types</div>
                 <div className="dropdown-content" id="listCakeType">
-                    <a onClick={(e) => console.log(e)} href="#">All</a>
-                    <a onClick={(e) => console.log(e)} href="#">Vanilla</a>
-                    <a onClick={(e) => console.log(e)} href="#">Chocolate</a>
-                    <a onClick={(e) => console.log(e)} href="#">Fruit</a>
+                    <a onClick={(e) => setCakeType(0)} href="#">All</a>
+                    <a onClick={(e) => setCakeType(2)} href="#">Vanilla</a>
+                    <a onClick={(e) => setCakeType(3)} href="#">Chocolate</a>
+                    <a onClick={(e) => setCakeType(1)} href="#">Fruit</a>
                 </div>
             </div>
             <div className="dropdown">
                 <div className="dropdown-button" id="btnCakeSize" onClick={toggleFilterMenu}>All Sizes</div>
                 <div className="dropdown-content" id="listCakeSize">
-                    <a onClick={(e) => console.log(e)} href="#">All</a>
-                    <a onClick={(e) => console.log(e)} href="#">Personal</a>
-                    <a onClick={(e) => console.log(e)} href="#">Small</a>
-                    <a onClick={(e) => console.log(e)} href="#">Large</a>
+                    <a onClick={(e) => setCakeSize(0)} href="#">All</a>
+                    <a onClick={(e) => setCakeSize(1)} href="#">Personal</a>
+                    <a onClick={(e) => setCakeSize(2)} href="#">Small</a>
+                    <a onClick={(e) => setCakeSize(3)} href="#">Large</a>
                 </div>
             </div>
         </div>
         <div id="cakecard-container">
-            {cakes.map((cake, index) => {
-                return <CakeCard key={index} {...cake} />;
-            })}
+            {cakes
+                .filter(cake => (cakeSize > 0) ? cake.size === cakeSize : cake)
+                .filter(cake => (cakeType > 0) ? cake.base_type === cakeType : cake)
+                .map((cake, index) => {
+                    return <CakeCard key={index} {...cake} />;
+                })
+            }
         </div>
     </div>
     )
