097bb384a81b114912ff9ff08b9203782652165b
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-07 17:00:01 -0400

    DB recycle

--- client/src/components/CakeCard.js
+++ client/src/components/CakeCard.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function CakeCard(props) {
 
+const image_url = "./cakes/thumbs/" + props.image;
+
 return (
     <div className="cakecard">
         <div className="cakecard-thumbnail-container">
-            <img src={props.image_url} alt={props.name} />
+            <img src={image_url} alt={props.name} />
         </div>
         <div className="cakecard-text">{props.name}</div>
     </div>
