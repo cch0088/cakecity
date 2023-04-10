d3869bca0441d664543c2a43f5f7c8c5005a4635
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-10 12:45:34 -0400

    Updates

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,6 +9,14 @@ return(
         <div id="home-nav-container">
             <NavBar/>
         </div>
+        <div className="home-slide">
+            <div>AB</div>
+            <div><img src="./cakecity/cakes/thumbs/medium-chocolate-cake.png" alt="Chocolate Cake" /></div>
+        </div>
+        <div className="home-slide">
+            <div><img src="./cakecity/cakes/thumbs/personal-fruit-cake.png" alt="Fruit Cake" /></div>
+            <div>AB</div>
+        </div>
     </div>
 )
 }
