cff3332da7e78739f2f951bcbd4f34b3e7d1680c
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-10 15:35:32 -0400

    Front page update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,12 +10,17 @@ return(
             <NavBar/>
         </div>
         <div className="home-slide">
-            <div>AB</div>
+            <div className="caption">Welcome to Cake City!</div>
+            <div className="caption">
+                The place for personalized cakes for birthdays or any occassion!
+                You can check out our menu or place an order today and choose when you want it delivered.
+                Get your special cake today. Cakes are made to order with special options available.
+            </div>
             <div><img src="./cakecity/cakes/thumbs/medium-chocolate-cake.png" alt="Chocolate Cake" /></div>
         </div>
         <div className="home-slide">
             <div><img src="./cakecity/cakes/thumbs/personal-fruit-cake.png" alt="Fruit Cake" /></div>
-            <div>AB</div>
+            <div>You can have your cake and eat it too!</div>
         </div>
     </div>
 )
