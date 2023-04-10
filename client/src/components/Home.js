744cc8898b1194fd9e71104b38b803e56ac8f36d
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-10 15:45:49 -0400

    Front page update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
 
 return(
     <div id="home-content">
-        <img src="./cakecity/logos/cakecitylogo.png" alt="Cake City Logo" />
+        <img src="./logos/cakecitylogo.png" alt="Cake City Logo" />
         <div id="home-nav-container">
             <NavBar/>
         </div>
