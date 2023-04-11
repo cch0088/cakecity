56dd42c7ef608554eee1f044b449e707e9c8b5ae
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-11 17:35:06 -0400

    Prop drill

--- client/src/components/CakeCard.js
+++ client/src/components/CakeCard.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function CakeCard(props) {
 
 const image_url = "./cakes/thumbs/" + props.image;
 
+let history = useHistory();
+
+function selectCake(e) {
+    props.setBuildCakeID(e.target.id);
+    history.push("/cakecity/cakebuilder");
+}
+
 return (
-    <div className="cakecard" id={props.id} onClick={(e) => props.setBuildCakeID(e.target.id)}>
+    <div className="cakecard" id={props.id} onClick={selectCake}>
         <div className="cakecard-thumbnail-container">
             <img src={image_url} alt={props.name} id={props.id} 
             onError={(e) => (e.target.src = "./cakes/thumbs/no-cake.png")} />
