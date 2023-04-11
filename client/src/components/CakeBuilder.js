87a564953c01735221b27651a4a8ad0eea410e9b
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-11 18:45:29 -0400

    Cake builder beta

--- client/src/components/CakeBuilder.js
+++ client/src/components/CakeBuilder.js
@@ -14,12 +14,33 @@ function CakeBuilder(props) {
 
 return (
     <div id="content">
-        <div className="cakecard" id={cake.id}>
-            <div className="cakecard-thumbnail-container">
-                <img src={image_url} alt={cake.name} id={cake.id}
+        <div>This section is under construction</div>
+        <div className="cakecard-large">
+            <div className="cakecard-text">{cake.name}</div>
+            <div>
+                <img src={image_url} alt={cake.name} 
                 onError={(e) => (e.target.src = "./cakes/thumbs/no-cake.png")} />
             </div>
-            <div className="cakecard-text">{cake.name}</div>
+        </div>
+        <div className="cakecard-large">
+            <div className="cakecard-text">Contents</div>
+            <div>
+                <ul>
+                    {cake.contents.map((x) => <li>{x}</li>)}
+                </ul>
+            </div>
+        </div>
+        <div className="cakecard-large">
+            <div className="cakecard-text">Personalize</div>
+            <div>
+                <ul>
+                    <li>Add candles</li>
+                    <li>Add text</li>
+                </ul>
+            </div>
+        </div>
+        <div className="cakecard-large">
+            <div className="cakecard-text">Place Order</div>
         </div>
     </div>
     )
