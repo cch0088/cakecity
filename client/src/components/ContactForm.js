f260c6c16fb820a50e6642a4e64d960a84ca2dda
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-12 16:59:27 -0400

    Bug fixes

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -5,13 +5,13 @@ function ContactForm() {
 return (
     <div id="site-form">
         <div className="title-label">CONTACT US</div>
-        <div className="label">Questions or comments?</div>
-        <div className="label">✉️ info@cakecity.biz</div>
-        <div className="label">☎️ (555) 555-5555</div>
-        <div className="label">Our Location</div>
+        <div className="label-login">Questions or comments?</div>
+        <div className="label-login">✉️ info@cakecity.biz</div>
+        <div className="label-login">☎️ (555) 555-5555</div>
+        <div className="label-login">Our Location</div>
         <img src="./photos/location.jpg" alt="Location" />
-        <div className="label">123 Fake St</div>
-        <div className="label">New York, NY</div>
+        <div className="label-login">123 Fake St</div>
+        <div className="label-login">New York, NY</div>
     </div>
     )
 }
