10cc888000af496d4d85c4246313153a757e5112
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-12 16:44:50 -0400

    Password reset dialog

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Registration from "./Registration";
 import Login from "./Login";
+import Reset from "./Reset";
 
 function Order() {
 
 return(
     <div id="content">
-        <Login />
+        <Reset />
     </div>
 )
 }
