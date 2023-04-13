5449615f52a2e01acdc98a921072d75e50f6cdf2
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-13 13:49:31 -0400

    Order page update

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import OrderCard from "./OrderCard";
 import Registration from "./Registration";
 import Login from "./Login";
@@ -6,10 +8,18 @@ import Reset from "./Reset";
 
 function Order() {
 
+    const API = "https://my-json-server.typicode.com/cch0088/cakecity/orders";
+
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        fetch(API).then(resp => resp.json()).then(data => setOrders(data));
+    }, []);
+
 return(
     <div id="content">
         <h2>Recent Orders</h2>
-        <OrderCard />
+        {orders.map((order, index) => <OrderCard key={index} {...order} />)}
     </div>
 )
 }
--- client/src/components/OrderCard.js
+++ client/src/components/OrderCard.js
@@ -1,32 +1,48 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function OrderCard(props) {
+
+    const [cake, setCake] = useState([]);
+    const API = "https://my-json-server.typicode.com/cch0088/cakecity/cakes/" + props.cake_id;
+
+    function capName(name) {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
+    function roundFloat(num) {
+        return (Math.round(num * 100) / 100).toFixed(2);        
+    }
+
+    useEffect(() => {
+        fetch(API).then(resp => resp.json()).then(data => setCake(data));
+    }, [API]);
 
 return(
     <div className="div-table">
         <div className="div-table-heading">
             <div className="div-table-cell">Order Number</div>
-            <div className="div-table-cell">001</div>
+            <div className="div-table-cell">{props.id}</div>
         </div>
         <div className="div-table-heading">
             <div className="div-table-cell">Cake Type</div>
-            <div className="div-table-cell">Medium Chocolate Cake</div>
+            <div className="div-table-cell">{cake.name}</div>
         </div>
         <div className="div-table-heading">
             <div className="div-table-cell">Ready Date</div>
-            <div className="div-table-cell">04/20/2023</div>
+            <div className="div-table-cell">{props.ready_date}</div>
         </div>
         <div className="div-table-heading">
             <div className="div-table-cell">Order Date</div>
-            <div className="div-table-cell">04/16/2023</div>
+            <div className="div-table-cell">{props.created_at}</div>
         </div>
         <div className="div-table-heading">
             <div className="div-table-cell">Total Price</div>
-            <div className="div-table-cell">$42.50</div>
+            <div className="div-table-cell">${roundFloat(props.total_price)}</div>
         </div>
         <div className="div-table-heading">
             <div className="div-table-cell">Delivery Type</div>
-            <div className="div-table-cell">Pickup</div>
+            <div className="div-table-cell">{capName(props.delivery)}</div>
         </div>
     </div>
 )
