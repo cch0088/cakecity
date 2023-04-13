02d18864f8c0aea80052d719f5e2095216795fea
Constantine Cherkashyn <cons1@protonmail.ch>
2023-04-12 20:55:59 -0400

    Order page

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -7,7 +7,33 @@ function Order() {
 
 return(
     <div id="content">
-        <Login />
+        Recent Orders
+        <div className="div-table">
+            <div className="div-table-heading">
+                <div className="div-table-cell">Order Number</div>
+                <div className="div-table-cell">001</div>
+            </div>
+            <div className="div-table-heading">
+                <div className="div-table-cell">Cake Type</div>
+                <div className="div-table-cell">Medium Chocolate Cake</div>
+            </div>
+            <div className="div-table-heading">
+                <div className="div-table-cell">Ready Date</div>
+                <div className="div-table-cell">04/20/2023</div>
+            </div>
+            <div className="div-table-heading">
+                <div className="div-table-cell">Order Date</div>
+                <div className="div-table-cell">04/16/2023</div>
+            </div>
+            <div className="div-table-heading">
+                <div className="div-table-cell">Total Price</div>
+                <div className="div-table-cell">$42.50</div>
+            </div>
+            <div className="div-table-heading">
+                <div className="div-table-cell">Delivery Type</div>
+                <div className="div-table-cell">Pickup</div>
+            </div>
+        </div>
     </div>
 )
 }
